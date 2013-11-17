8db31a111c27c9bfc7fdd3783b8612208107f0a7
dozencrows <git@magwitch.uk.net>
2013-11-17 17:55:35 +0000

    Fixed stills mode, some tidying

--- mmalcam.h
+++ mmalcam.h
@@ -29,6 +29,8 @@ typedef struct mmalcam_context {
     struct MMAL_POOL_T *camera_buffer_pool;
     struct MMAL_QUEUE_T *camera_buffer_queue;
     struct raspicam_camera_parameters_s *camera_parameters;
+    struct MMAL_COMPONENT_T *preview_component;
+    struct MMAL_CONNECTION_T *preview_connection;
 
     void (*camera_buffer_callback)(struct MMAL_PORT_T *port, struct MMAL_BUFFER_HEADER_T *buffer);
 } mmalcam_context;
